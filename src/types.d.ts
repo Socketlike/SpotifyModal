0ab29900800b1cb43b098bebbb01701c98336496
Socketlike <36399055+Socketlike@users.noreply.github.com>
2023-03-30 19:13:35 +0700

    fix(lint): fix eslint warnings about types.d.ts' unused vars

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/naming-convention, @typescript-eslint/no-unused-vars */
 
 namespace Spotify {
   interface PluginWS extends WebSocket {
