d55f0879d5dfc674ffc8efa471c17d0085496da7
Socketlike <36399055+Socketlike@users.noreply.github.com>
2023-06-29 10:50:32 +0700

    chore(components/Settings): use lodash to create markers

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -3,7 +3,7 @@ import { events, useTrappedSettingsState } from '@?util';
 import { DefaultConfigType, DefaultConfigTypeKeys, config } from '@config';
 
 const { FormItem, SelectItem, Slider, SwitchItem } = components;
-const { React } = common;
+const { React, lodash: _ } = common;
 
 const updateSetting = <T extends DefaultConfigTypeKeys, D extends DefaultConfigType[T]>(
   key: T,
@@ -81,10 +81,7 @@ export const Settings = (): JSX.Element => {
           disabled={!config.get('skipPreviousShouldResetProgress')}
           minValue={0}
           maxValue={1}
-          markers={[
-            0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75,
-            0.8, 0.85, 0.9, 0.95, 1,
-          ]}
+          markers={_.range(0, 1.05, 0.05).map((v) => Number(v.toFixed(2)))}
           {...useTrappedSettingsState(
             util.useSetting(config, 'skipPreviousProgressResetThreshold'),
             'skipPreviousProgressResetThreshold',
